df1d2f31804ec880bf027a44a9e3b53b6fb08dc7
sumit-sharm3-shell <86064824+sumit-sharm3-shell@users.noreply.github.com>
2023-09-21 16:50:04 +0530

    Update ec2.tf

--- starter/terraform/ec2.tf
+++ starter/terraform/ec2.tf
@@ -11,7 +11,7 @@
    source             = "./modules/ec2"
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0e65d1c5f6668191a"
+   aws_ami            = "ami-019aad7f8b93f0e47"
    private_subnet_ids = module.vpc.private_subnet_ids
    vpc_id             = module.vpc.vpc_id
  }
