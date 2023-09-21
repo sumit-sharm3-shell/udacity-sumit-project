4cb601337cb756400a930149ce6ab5b47450f01a
sumit-sharm3-shell <86064824+sumit-sharm3-shell@users.noreply.github.com>
2023-09-21 18:59:00 +0530

    Update _config.tf

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-terraform-east-1"
+     bucket = "udacity-terraform-useast-1"
      key    = "terraform/terraform.tfstate"
      region = "us-east-1"
    }
